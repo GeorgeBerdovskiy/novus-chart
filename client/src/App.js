d64af870e111576ebfa5f0726ebc582578e23b36
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-06-29 19:13:02 -0700

    Start client

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Novus Chart</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
