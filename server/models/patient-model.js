cf42861f697f118129232ffd8267693225a605bb
GeorgeBerdovskiy <georgeberdovskiy@Georges-iMac.local>
2021-06-29 21:30:35 -0700

    Update Patient Schema to Reflect Test Results

--- server/models/patient-model.js
+++ server/models/patient-model.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
+const TestResult = require('test-result-model')
+
 const Patient = new Schema(
     {
-        first_name: { type: String, required: true },
-        last_name: { type: String, required: true },
+        status: { type: String, required: true },
+        title: { type: String, required: true },
+        description: { type: String, required: true },
     },
 )
 
