a519457a85b2fe1d8cb46900e5304c94636c3202
Shuzhengz <treez.zhang@gmail.com>
2021-06-29 22:27:53 -0700

    fix more issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 import "./css/App.css";
-import "./css/applet.css";
-import "./css/dashboard.css";
 
 import Dashboard from "./Components/Pages/dashboard";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Appointments from "./Components/Elements/appointments";
-import TestResults from "./Components/Elements/testResults";
-import Prescriptions from "./Components/Elements/prescriptions";
-import SymptomTracker from "./Components/Elements/symptomTracker";
-import Messages from "./Components/Elements/messages";
-import Payment from "./Components/Elements/payment";
+import Appointments from "./Components/Pages/Appointments";
 
 function App() {
   return (
@@ -17,19 +10,21 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <h1>Novus Chart</h1>
-                <Dashboard />
             </header>
                 <Switch>
+                    <Route exact path="/" component={Dashboard}/>
                     <Route exact path="/appointments" component={Appointments}/>
-                    <Route exact path="/test_results" component={TestResults}/>
-                    <Route exact path="/prescriptions" component={Prescriptions}/>
-                    <Route exact path="/symptom_tracker" component={SymptomTracker}/>
-                    <Route exact path="/messages" component={Messages}/>
-                    <Route exact path="/payment" component={Payment}/>
                 </Switch>
             </div>
       </BrowserRouter>
   );
 }
+
+// TODO implement later
+//<Route exact path="/test_results" component={TestResults}/>
+//<Route exact path="/prescriptions" component={Prescriptions}/>
+//<Route exact path="/symptom_tracker" component={SymptomTracker}/>
+//<Route exact path="/messages" component={Messages}/>
+//<Route exact path="/payment" component={Payment}/>
 
 export default App;
